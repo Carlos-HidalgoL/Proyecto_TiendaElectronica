1cb8cdabc479658e762610342f012df1aa61b0d0
Kenneth-Navarro <dbarrantesq17@gmail.com>
2024-06-20 22:06:17 -0600

    Modelo de Detalle Factura y Articulo Factura agregados

---
+++ Models/ArticuloFactura.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyecto_TiendaElectronica.Models
+{
+    public class ArticuloFactura
+    {
+        public int idDetalleFactura { get; set; }
+
+        public int idArticulo { get; set; }
+
+        public int CantidadArticulo { get; set; }
+
+
+    }
+}
---
+++ Models/DetalleFactura.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto_TiendaElectronica.Models
+{
+    public class DetalleFactura
+    {
+        public int IdDetalleFactura { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public decimal MontoTotal { get; set; }
+
+    }
+}
