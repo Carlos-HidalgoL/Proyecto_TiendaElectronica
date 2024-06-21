45484cde86c1f73251b7558be1b7842e0cd77ad4
Carlos-HidalgoL <carloshl349@gmail.com>
2024-06-20 22:03:15 -0600

    Modelos de Factura y Usuario agregados

---
+++ Models/Factura.cs
@@ -0,0 +1,17 @@
+﻿namespace Proyecto_TiendaElectronica.Models
+{
+    public class Factura
+    {
+        public int IdFactura { get; set; }
+        public DateTime FechaCrecion { get; set; }
+
+        public DateTime UltimaFechaImpresion { get; set; }
+
+        public string IdUsuario { get; set; }
+
+        public int idDetalleFactura { get; set; }
+
+                                                  
+    }                                             
+}                                                 
+                                                  
---
+++ Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿namespace Proyecto_TiendaElectronica.Models
+{
+    public class Usuario
+    {
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+        
+        public string Correo { get; set; }
+        
+        public string Contrasena { get; set; }
+
+        public bool Estado { get; set; }
+
+        public string Telefono { get; set; }
+
+
+
+    }
+}
